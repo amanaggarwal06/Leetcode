eadb5af9eeb0102711d63f97c32d514909eef63b
Aman Aggarwal <116307103+amanaggarwal06@users.noreply.github.com>
2024-03-12 02:43:01 +0530

    Time: 4 ms (26.84%) | Memory: 7.8 MB (49.48%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        
+        //brute force--> make an temp array and preserve the order and then apply any sorting algorithm to sort our required string s
+
+         std::unordered_map<char, int> charCount;
+        for (char c : order) {
+            charCount[c] = 0;
+        }
+        
+        for (char c : s) {
+            if (charCount.find(c) != charCount.end()) {
+                charCount[c]++;
+            }
+        }
+        
+        std::string sortedS;
+        for (char c : order) {
+            sortedS.append(charCount[c], c);
+        }
+        
+        for (char c : s) {
+            if (charCount.find(c) == charCount.end()) {
+                sortedS.push_back(c);
+            }
+        }
+        
+        return sortedS;
+    }
+};
