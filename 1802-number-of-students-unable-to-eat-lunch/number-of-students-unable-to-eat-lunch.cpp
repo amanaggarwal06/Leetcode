b075f997844974b28f22e478957fdc9edfe66737
Aman Aggarwal <116307103+amanaggarwal06@users.noreply.github.com>
2024-04-08 23:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (79.46%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    // int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        
+    //     int total_stud = stud.size(), total_sand = sandwiches.size();
+
+        
+    // }
+        int countStudents(vector<int>& A, vector<int>& B) {
+            int count[] = {0, 0}, n = A.size(), k;
+            for (int a : A)
+                count[a]++;
+            for (k = 0; k < n && count[B[k]] > 0; ++k)
+                count[B[k]]--;
+            return n - k;
+    }
+};
